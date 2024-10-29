0788b6b578686dba4834a228b52eda03270e42bc
ZoDream <648383079@qq.com>
2024-10-29 12:34:45 +0800

    update

--- forum/src/js/main.ts
+++ forum/src/js/main.ts
@@ -7,5 +7,18 @@ $(function() {
             $('.dropdown-container').removeClass('popup-open');
         }
         box.toggleClass('popup-open', !toggle);
+        $('.user-popup').removeClass('popup-open');
+    }).on('click', '.user-control-icon', function(e) {
+        e.preventDefault();
+        const $this = $(this);
+        const pos = $this.offset();
+        $('.user-popup').addClass('popup-open').css({
+            left: pos.left - 80 + 'px',
+            top: pos.top + 40 + 'px',
+        });
+    }).on('click', '.command-bar .command-control-icon', function() {
+        $(this).closest('.command-bar').toggleClass('popup-open');
+    }).on('click', '.header .search-bar .search-control-icon', function() {
+        $(this).closest('.header').toggleClass('search-toggle-mode');
     });
 });
